c579b1a5dc7d89cb4f0c2b9fcfc9dbe47abd1bc6
Aries0331 <jinzhu@ualberta.ca>
2016-11-18 23:17:28 -0700

    update from dropbox

--- Tilecoder.py
+++ Tilecoder.py
@@ -11,10 +11,10 @@ def tilecode(in1,in2,tileIndices):
 def printTileCoderIndices(in1,in2):
     tileIndices = [-1]*numTilings
     tilecode(in1,in2,tileIndices)
-    print('Tile indices for input (',in1,',',in2,') are : ', tileIndices)
+    print('Tile indices for input (', in1, ',', in2,') are : ', tileIndices)
 
 printTileCoderIndices(0.1,0.1)
 printTileCoderIndices(4.0,2.0)
 printTileCoderIndices(5.99,5.99)
 printTileCoderIndices(4.0,2.1)
-    
+
